63490a1ed23a6919b2b8ba7f1f379364640734cb
Aiden <aiden@oldstreetjournal.co.uk>
2023-03-28 11:45:31 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     flake-utils.url = "github:numtide/flake-utils";
     voidrice.url = "github:LukeSmithxyz/voidrice";
     voidrice.flake = false;
